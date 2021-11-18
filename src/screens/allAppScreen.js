92acc01e98453d63905f1792a678c301d5dbad37
joker <p.sangsawai@gmail.com>
2021-11-18 17:05:16 +0700

    Update allAppScreen.js

--- src/screens/allAppScreen.js
+++ src/screens/allAppScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const AllAppScreen = () => {
